1075ad91cfb2c322480424638afbb81b8b426600
Shivam Patel <shivampatel251097@gmail.com>
2021-01-03 00:36:46 +0530

    Promises

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const dbOper = require('./operations');
 const url = 'mongodb://localhost:27017/';
 const dbname = 'confusion';
 
-MongoClient.connect(url,(err,client)=>{
-    assert.strictEqual(err,null);
+// MongoClient.connect(url,(err,client)=>{
+//     assert.strictEqual(err,null);
 
-    console.log('Connected to server successfully');
-    const db = client.db(dbname);
+//     console.log('Connected to server successfully');
+//     const db = client.db(dbname);
     // const collection =  db.collection('dishes');
 
     // collection.insertOne({"name":"Shivam","Description":"Hello there, I'm Shivam Patel!"},(err, result)=>{
@@ -30,24 +30,60 @@ MongoClient.connect(url,(err,client)=>{
     //         })
     //     });
     // });
-    dbOper.insertDocument(db, {name: "Vasundhara", description: 'Good '},'dishes', (result)=>{
+
+    // Using Callbacks
+//     dbOper.insertDocument(db, {name: "Vasundhara", description: 'Good '},'dishes', (result)=>{
+//         console.log('Insert Document:\n',result.ops);
+
+//         dbOper.findDocuments(db,'dishes',(docs)=>{
+//             console.log("Found documents:\n",docs);
+//             dbOper.updateDocument(db,({name: 'Vasundhara'}), {description: 'Updated Good'}, 'dishes', (result)=>{
+//                 console.log('Updated Document:\n', result.result);
+
+//                 dbOper.findDocuments(db,'dishes',(docs)=>{
+//                     console.log("Found documents:\n",docs);
+
+//                     db.dropCollection('dishes',(result)=>{
+//                         console.log('Dropped Collection: ',result);
+
+//                         client.close();
+//                     });
+//                 });
+//             });
+//         });
+//     });
+// });
+
+// Using promises to reduce callback complexity
+MongoClient.connect(url).then((client)=>{
+    console.log('Connected to server successfully');
+    const db = client.db(dbname);
+ 
+    dbOper.insertDocument(db, {name: "Vasundhara", description: 'Good '},'dishes') 
+    .then((result)=>{
         console.log('Insert Document:\n',result.ops);
 
-        dbOper.findDocuments(db,'dishes',(docs)=>{
+        return dbOper.findDocuments(db,'dishes');
+    })
+        .then((docs)=>{
             console.log("Found documents:\n",docs);
-            dbOper.updateDocument(db,({name: 'Vasundhara'}), {description: 'Updated Good'}, 'dishes', (result)=>{
+            return dbOper.updateDocument(db,({name: 'Vasundhara'}), {description: 'Updated Good'}, 'dishes')
+        })
+             .then((result)=>{
                 console.log('Updated Document:\n', result.result);
 
-                dbOper.findDocuments(db,'dishes',(docs)=>{
+                return dbOper.findDocuments(db,'dishes');
+             })
+                .then((docs)=>{
                     console.log("Found documents:\n",docs);
 
-                    db.dropCollection('dishes',(result)=>{
+                    return db.dropCollection('dishes');
+                })
+                    .then((result)=>{
                         console.log('Dropped Collection: ',result);
 
-                        client.close();
-                    });
-                });
-            });
-        });
-    });
-});
+                        return client.close();
+                    })
+                    .catch((err)=> console.log(err));
+})
+.catch((err) => console.log(err));
